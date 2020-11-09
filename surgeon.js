6e614d61799d1b0188954d22b5a831e4c2eacc87
Kevin Ho <kevinjho1996@gmail.com>
2020-11-09 16:43:24 -0500

    add abi to dump

--- surgeon.js
+++ surgeon.js
@@ -14,12 +14,14 @@ for(let contractName in synthethixDeployment.targets) {
   const contractAddress = snxContract.address
   const v2ContractFile = JSON.parse(fs.readFileSync('compiled/' + snxContract.source + '.json'))
   const updatedCode = v2ContractFile.evm.deployedBytecode.object
+  const abi = v2ContractFile.abi
   const account = testnetDump.result.accounts[contractAddress.toLowerCase()]
-  // console.log(Object.keys(account))
+
   const updatedAccount = {
     address: contractAddress,
     nonce: account.nonce,
     code: '0x' + updatedCode,
+    abi
   }
   if(account.storage) {
     Object.keys(account.storage).map(function(key, index) {
@@ -32,16 +34,17 @@ for(let contractName in synthethixDeployment.targets) {
 for (const [address, account] of Object.entries(testnetDump.result.accounts)) {
   // console.log(value)
   if(account.codeHash === 'c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470') {
-    const eoaBytecode = JSON.parse(fs.readFileSync('OVM_ProxyEOA.json')).deployedBytecode
+    const {deployedBytecode, abi} = JSON.parse(fs.readFileSync('OVM_ProxyEOA.json'))
     const eoaPrecompileAddress = '0x4200000000000000000000000000000000000003'
     const eoaName = 'EOA_' + address
     newStateDump.accounts[eoaName] = {
       address: address,
       nonce: account.nonce,
-      code: eoaBytecode,
+      code: deployedBytecode,
       storage: {
         "0x0000000000000000000000000000000000000000000000000000000000000000": eoaPrecompileAddress,
-      }
+      },
+      abi
     }
   }
 
